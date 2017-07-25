b71b1f4ea5c3680153ecc38f27f5335ee952d573
TimothyJamesAitchison <Timothy.Aitchison@protonmail.com>
2017-07-25 13:54:28 +1200

    Add files via upload

---
+++ Exercise4/Exercise4/Model.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    class Model
+    {
+        public int GetSum(int max)
+        {
+            int sum = 0;
+            for(int i = 1; i <= 100; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+
+        public int GetAverage(int max)
+        {
+            return this.GetSum(max) / max;
+        }
+    }
+}
